9c630b37643afd35fb5b2a59d0fefdb2940ed512
allison-forsyth <aforsyt4@gmu.edu>
2025-02-19 17:44:38 -0500

    Ran all api calls fro myc grid and saved all as geotiffs in folders

--- process_api_connection.py
+++ process_api_connection.py
@@ -31,8 +31,8 @@ from sentinelhub import SHConfig
 
 
 config = SHConfig()
-config.sh_client_id = '<client id>'
-config.sh_client_secret = '<client secret>'
+config.sh_client_id = 'sh-ac01828e-eb8f-4ff4-b9de-7630c5312236'
+config.sh_client_secret = '2NHA7aykyeBqu3NZnrI7n9eVU5NU8oFb'
 config.sh_base_url = 'https://sh.dataspace.copernicus.eu'
 config.sh_token_url = 'https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token'
 config.save("cdse")
@@ -46,7 +46,7 @@ create eval script to downolad and grab images of interest
 current eval script is the VH decible 0 Gamma
 """
 
-evalscript = """
+evalscriptVH = """
 //VERSION=3
 function setup() {
   return {
@@ -79,6 +79,36 @@ return [Math.max(0, Math.log(VH) * 0.21714724095 + 1)];
 
 """
 
+evalscriptVV = """ 
+//VERSION=3 
+function setup() { 
+  return {
+    input: ["VV", "dataMask"], 
+    output: [ 
+      { id: "default", bands: 4 }, 
+      { id: "eobrowserStats", bands: 1 }, 
+      { id: "dataMask", bands: 1 }, 
+    ], 
+  }; 
+} 
+function evaluatePixel(samples) { 
+  const value = Math.max(0, Math.log(samples.VV) * 0.21714724095 + 1); 
+  return { 
+    default: [value, value, value, samples.dataMask], 
+    eobrowserStats: [Math.max(-30, (10 * Math.log10(samples.VV)))], 
+    dataMask: [samples.dataMask], 
+  }; 
+} 
+// --- 
+/* 
+  // displays VV in decibels from -20 to 0 
+  // the following is simplified below 
+  // var log = 10 * Math.log(VV) / Math.LN10; 
+  // var val = Math.max(0, (log + 20) / 20); 
+  return [Math.max(0, Math.log(VV) * 0.21714724095 + 1)]; 
+*/ 
+""" 
+
 """
 send request
 """
@@ -86,10 +116,12 @@ send request
 # copy and pase from excel sheet be sure to include commas between values
 
 #this is changing depending on grid being pulled
-bbox = BBox(bbox=[-7377043.971, 2092185.762, -7372043.971, 2087185.762], crs=CRS.POP_WEB)
+bbox1 = BBox(bbox=[-7392043.971,	2092185.762,	-7387043.971,	2087185.762], crs=CRS.POP_WEB)
+bbox2 = BBox(bbox=[-7387043.971,	2092185.762, -7382043.971,	2087185.762], crs=CRS.POP_WEB)
+bbox3 = BBox(bbox=[-7382043.971,	2092185.762,	-7377043.971,	2087185.762], crs=CRS.POP_WEB)
+bbox4 = BBox(bbox=[-7377043.971,	2092185.762,	-7372043.971,	2087185.762], crs=CRS.POP_WEB)
+bbox5 = BBox(bbox=[-7372043.971,	2092185.762,	-7367043.971,	2087185.762], crs=CRS.POP_WEB)
 
-#pre flood time: '2017-09-01', '2017-09-16'
-#post flood time: '2017-09-20', '2017-09-28'
 
 # namming convention
 # preflood_VV_gridnumber
@@ -97,22 +129,25 @@ bbox = BBox(bbox=[-7377043.971, 2092185.762, -7372043.971, 2087185.762], crs=CRS
 # preflood_VH_gridnumber
 # postflood_VH_gridnumber
 
+#pre flood time: '2017-09-01', '2017-09-16'
+#post flood time: '2017-09-20', '2017-09-28'
+
 request = SentinelHubRequest(
-    data_folder="testing2", #this is changing every pull
-    evalscript=evalscript,
+    data_folder="postflood_VV_05", #this is changing every pull
+    evalscript=evalscriptVV,
     input_data=[
         SentinelHubRequest.input_data(
             data_collection=DataCollection.SENTINEL1_IW.define_from(
                     "s1iw", service_url=config.sh_base_url
                 ),          
-            time_interval=('2017-09-01', '2017-09-16'), #this is chnaging depending on pre or post flood         
+            time_interval=('2017-09-20', '2017-09-28'), #this is chnaging depending on pre or post flood         
             other_args={"dataFilter": {"mosaickingOrder": "mostRecent"},"processing": {"backCoeff": "GAMMA0_TERRAIN","orthorectify": True,"demInstance": "COPERNICUS","speckleFilter": {"type": "LEE","windowSizeX": 3,"windowSizeY": 3}}}
         ),
     ],
     responses=[
         SentinelHubRequest.output_response('default', MimeType.TIFF),
     ],
-    bbox=bbox,
+    bbox=bbox5,
     size=[474.37555468171587, 474.3100863719954],
     config=config
 )
@@ -124,10 +159,15 @@ save geotiff to folder
 """
 
 #%%time
-vh_layer = request.get_data(save_data=True)
+layer = request.get_data(save_data=True)
 for folder, _, filenames in os.walk(request.data_folder):
     for filename in filenames:
         print(os.path.join(folder, filename))
+
+layer
+
+
+
 
 
 
